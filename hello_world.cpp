548bdd3b67ccc5815801780195501158987bd771
The BlueFlex <76541836+BlueFlex@users.noreply.github.com>
2021-01-12 19:20:14 -0300

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+  
+  using namespace std;
+
+  int main()
+  {
+      string s;
+      s = "Hello world";
+      cout << s << endl;
+      system("PAUSE");
+      return 0;
+  }
